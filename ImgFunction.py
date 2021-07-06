d74dda7ce7bd353a4d9820fdf26af2bd9fd9a1a4
Charles Wu <lineup730@gmail.com>
2021-07-06 23:15:23 +0800

    add modify lightness and saturation

--- ImgFunction.py
+++ ImgFunction.py
@@ -70,3 +70,50 @@ def modify_intensity(img, scale):
     process_img = np.array(process_img, dtype=np.uint8)
 
     return process_img
+
+
+'''
+scale:  -scale percent ~ +scale persent
+'''
+def modify_lightness(img, scale):
+    # 圖像歸一化，且轉換為浮點型
+    fImg = img.astype(np.float32)
+    fImg = fImg / 255.0
+
+    # 顏色空間轉換 BGR -> HLS
+    hlsImg = cv2.cvtColor(fImg, cv2.COLOR_BGR2HLS)
+    hlsCopy = np.copy(hlsImg)
+
+    # 亮度調整
+    hlsCopy[:, :, 1] = (1 + scale / 100.0) * hlsCopy[:, :, 1]
+    hlsCopy[:, :, 1][hlsCopy[:, :, 1] > 1] = 1  # 應該要介於 0~1，計算出來超過1 = 1
+
+    # 顏色空間反轉換 HLS -> BGR 
+    result_img = cv2.cvtColor(hlsCopy, cv2.COLOR_HLS2BGR)
+    result_img = ((result_img * 255).astype(np.uint8))
+
+    return result_img
+
+
+'''
+scale:  -scale percent ~ +scale persent
+'''
+def modify_saturation(img, scale):
+
+    # 圖像歸一化，且轉換為浮點型
+    fImg = img.astype(np.float32)
+    fImg = fImg / 255.0
+
+    # 顏色空間轉換 BGR -> HLS
+    hlsImg = cv2.cvtColor(fImg, cv2.COLOR_BGR2HLS)
+    hlsCopy = np.copy(hlsImg)
+
+    # 飽和度調整
+    hlsCopy[:, :, 2] = (1 + scale / 100.0) * hlsCopy[:, :, 2]
+    hlsCopy[:, :, 2][hlsCopy[:, :, 2] > 1] = 1  # 應該要介於 0~1，計算出來超過1 = 1
+
+    # 顏色空間反轉換 HLS -> BGR 
+    result_img = cv2.cvtColor(hlsCopy, cv2.COLOR_HLS2BGR)
+    result_img = ((result_img * 255).astype(np.uint8))
+
+    return result_img
--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from ImgFunction import *
 
 img = cv2.imread('img/fly.jpg')
 resize_img = resize_img(img, 10)
-rotate_img = rotate_img(resize_img, 90);
-crop_img = crop_img(img, 500, 900, 100, 900)
-mi_img = modify_intensity(resize_img, 2)
+#rotate_img = rotate_img(resize_img, 90);
+#crop_img = crop_img(img, 500, 900, 100, 900)
+#mi_img = modify_intensity(resize_img, 2)
+light_img = modify_lightness(resize_img, 100)
+saturation_img = modify_saturation(resize_img, 100)
+
 
 cv2.imshow('fly', resize_img)
-cv2.imshow('mi_fly3', mi_img)
+cv2.imshow('light_img', light_img)
+cv2.imshow('saturation_img', saturation_img)
+
 '''
 cv2.imshow('fly2', rotate_img)
 cv2.imshow('fly3', crop_img)
